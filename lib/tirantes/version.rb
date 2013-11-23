8f7030e9492185732f91954d8897c1798fed6b83
Mario A Chavez <mario.chavez@gmail.com>
2013-11-23 16:53:13 -0600

    Update test_helper Update gitignore Bumped version Comment SMACSS structure load

--- lib/tirantes/version.rb
+++ lib/tirantes/version.rb
@@ -1,3 +1,3 @@
 module Tirantes
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
 end
--- templates/test_helper.rb
+++ templates/test_helper.rb
@@ -5,7 +5,7 @@ ENV['RAILS_ENV'] = 'test'
 
 require File.expand_path('../../config/environment', __FILE__)
 
-require 'minitest/autorun'
+require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/rails/capybara'
 require 'minitest/focus'
